003ca3c6fa63f06e78ce9407acee98f6c1bafd84
Deepak6264 <singhrathoredeepak661@gmail.com>
2025-07-24 17:23:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+import AdminLogin from "./components/admin/adminlogin/AdminLogin";
+import Dashboard from "./components/admin/adminlogin/Dashboard";
+import HomePage from "./components/userinterface/homepage/HomePage"
+import PageCategoryDisplay from "./components/userinterface/pagedisplay/PageCategoryDisplay";
+import ProductDetailPage from "./components/userinterface/productdetailspage/ProductDetailPage"
+import CartDisplayPage from "./components/userinterface/mycart/CartDisplayPage"
+import Login from "./components/userinterface/signin/Login"
+import Otp from "./components/userinterface/signin/Otp"
+import MakePayment from "./components/userinterface/mycart/MakePayment"
+import SetUp  from "./components/userinterface/signin/SetUp"
+function App() {
+  return (
+   <div>
+    <Router>
+      <Routes>
+        <Route element={<AdminLogin/>} path="/adminlogin"></Route>
+        <Route element={<Dashboard/>} path="/dashboard/*"></Route>
+        <Route element={<HomePage/>} path="/homepage"></Route>
+        <Route element={<PageCategoryDisplay/>} path="/pagecategorydisplay"></Route>
+        <Route element={<ProductDetailPage/>} path="/productdetailpage"></Route>
+        <Route element={<CartDisplayPage/>} path="/cartdisplaypage"></Route>
+        <Route element={<Login/>} path="/login"></Route>
+        <Route element={<Otp/>} path="/otp"></Route>
+        <Route element={<SetUp/>} path="/setup"></Route>
+        <Route element={<MakePayment/>} path="/makepayment"></Route>
+
+
+      </Routes>
+    </Router>
+   
+   </div>
+  );
+}
+
+export default App;
